00988075e8ac7d667e3cfafe4f06b13bbf44f694
shanmukh-sai2003 <104721888+shanmukh-sai2003@users.noreply.github.com>
2023-05-04 22:52:35 +0530

    sign up typo updated

--- pedia/urls.py
+++ pedia/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("login/", views.login_view, name = "login"),
     path("<str:name>", views.fullPage, name = "fullPage"),
     path('logout/', views.logout_view, name="logout"),
-    path('signup/', views.singup, name = 'singup'),
+    path('signup/', views.signup, name = 'signup'),
     path('add/', views.add, name = "add"),
 ]
 
